95e4374df87dfc5d23f2036f08d3c5e0ff01fbda
Bhavik Parmar <bhavik2936@gmail.com>
2020-05-16 01:47:49 +0530

    added web
    JSP support added

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//works as a REST Controller
-@RestController
+//works as a Controller
+@Controller
 public class UserController {
 
-//	serves GET method of HTTP for base URL / 
+//	serves GET method of HTTP for base URL '/'
 	@GetMapping("/")
 	public String dashboard() {
-		return "This is my Blog, and It works";
+		return "Welcome";
 	}
 }
