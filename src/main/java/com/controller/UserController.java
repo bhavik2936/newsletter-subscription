5a0aa15adf5ddf855ac897029a435c71844247b8
Bhavik Parmar <bhavik2936@gmail.com>
2020-05-23 22:03:12 +0530

    session enabled
    session added for admin panel

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -2,19 +2,21 @@ package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.tags.form.ErrorsTag;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bean.AdminBean;
 import com.dao.AdminDao;
 import com.dao.UserDao;
 
-//works as a Controller
+//works as a Controller and defining session attributes
 @Controller
+@SessionAttributes("adminBean")
 public class UserController {
-	
+
 	@Autowired
 	UserDao userDao;
 	@Autowired
@@ -25,26 +27,43 @@ public class UserController {
 	public String dashboard() {
 		return "Dashboard";
 	}
-	
+
+//	setting model attribute
+	@ModelAttribute("adminBean")
+	public AdminBean setModelAttributeAdminBean(AdminBean adminBean) {
+		return adminBean;
+	}
+
 //	admin panel login
 	@GetMapping("/admin")
-	public String adminPanel() {
-		return "AdminPanel";
-	}
-	
-//	admin panel access
-	@PostMapping("/admin")
-	public String authenticateAdmin(AdminBean adminBean) {
+	public String adminPanel(@ModelAttribute("adminBean") AdminBean adminBean) {
 		if (adminDao.authenticate(adminBean)) {
 			return "redirect:/newsletter";
-		} else {
-			return "AdminPanel";
 		}
+		return "AdminPanel";
 	}
-	
+
+//	admin panel access
+	@PostMapping("/admin")
+	public String authenticateAdmin(@ModelAttribute("adminBean") AdminBean adminBean) {
+		if (adminDao.authenticate(adminBean)) {
+			return "redirect:/newsletter";
+		}
+		return "AdminPanel";
+	}
+
 //	topic publishment model
 	@GetMapping("/newsletter")
-	public String newsletter() {
-		return "Newsletter";
+	public String newsletter(@ModelAttribute("adminBean") AdminBean adminBean) {
+		if (adminDao.authenticate(adminBean)) {
+			return "Newsletter";
+		}
+		return "redirect:/admin";
+	}
+
+	@GetMapping("/logout")
+	public String logout(SessionStatus status) {
+		status.setComplete();
+		return "redirect:/admin";
 	}
 }
